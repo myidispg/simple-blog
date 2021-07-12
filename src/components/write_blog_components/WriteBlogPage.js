2b71b5a2f021fd9539ab862bbfdab90a5b0bba47
Prashant Goyal <myidispg@gmail.com>
2021-07-12 21:13:32 +0530

    Added editing existing content functionality

--- src/components/write_blog_components/WriteBlogPage.js
+++ src/components/write_blog_components/WriteBlogPage.js
@@ -12,10 +12,10 @@ function WriteBlogPage() {
         date: `${today.toLocaleString('default', { month: "long" })} ${today.getDate()}, ${today.getFullYear()}`,
         author: "Prashant Goyal",
         title: "",
-        contentArray: []
+        contentArray: ["Hello", "My", "Name", "Is", "John", "Doe"]
     });
 
-    function handleInput(event) {
+    async function handleInput(event) {
         // Expand height of a text area based on the input
         let textArea = event.target;
         textArea.style.height = ""
@@ -24,6 +24,7 @@ function WriteBlogPage() {
         const targetName = event.target.name;
         const targetValue = event.target.value;
 
+        console.log(targetName);
         console.log(targetValue);
 
         if (targetName === "blog-title") {
@@ -33,7 +34,23 @@ function WriteBlogPage() {
                     title: targetValue,
                 }
             });
-        }       
+        } else if (targetName.includes("blog-content-para")) {
+            let indexOfContent = targetName.split("-")[3];
+            let newContentArray = blogContent.contentArray;
+
+            if(indexOfContent === undefined){
+                newContentArray.push(targetValue);
+            }else{
+                newContentArray[indexOfContent] = targetValue;
+            }
+            setBlogContent(prevValue => {
+                return {
+                    ...prevValue,
+                    contentArray: newContentArray
+                }
+            })
+        }
+        console.log(blogContent);
     }
 
     return <div>
@@ -41,7 +58,7 @@ function WriteBlogPage() {
         <div className="container-fluid">
             <form>
                 <div className="row">
-                    <textarea rows="1" type="text" placeholder="The Awesome Title" id="blog-title" name="blog-title" className="mx-auto expanding-text-area write-blog-heading" onInput={handleInput} spellCheck="false" />
+                    <textarea rows="1" type="text" placeholder="An Awesome Title" id="blog-title" name="blog-title" className="mx-auto expanding-text-area write-blog-heading" onInput={handleInput} spellCheck="false" />
                 </div>
                 {/* The Author date and horizontal rule */}
                 <div className="row blog-page-author-date">
@@ -55,7 +72,12 @@ function WriteBlogPage() {
                 </div>
                 <hr className="side-space read-blog-divider" />
                 {/* The blog content */}
-                <div className="row">
+                <div className="row" style={{ marginBottom: "50px" }}>
+                    {blogContent.contentArray.map((element, index) => {
+                        return <textarea key={index} value={element} rows="1" type="text" id="blog-content-para" name={`blog-content-para-${index}`} className="mx-auto expanding-text-area write-blog-paragraph" onInput={handleInput}>
+                            {element}
+                        </textarea>
+                    })}
                     <textarea rows="1" type="text" placeholder="Start writing here" id="blog-content-para" name="blog-content-para" className="mx-auto expanding-text-area write-blog-paragraph" onInput={handleInput} />
                 </div>
             </form>
