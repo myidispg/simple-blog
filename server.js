24c3b87778e68731849f16f9a6b6b12f6a21c41a
Prashant Goyal <myidispg@gmail.com>
2021-07-22 17:53:02 +0530

    Added endpoint to get all blogs from the backend

--- server.js
+++ server.js
@@ -1,8 +1,22 @@
+// import allBlogsData from "./src/all_blogs";
+
 const express = require('express');
 const app = express();
+
+app.set('view engine', 'ejs');
+
+// app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static("public"));
+
 const port = process.env.PORT || 5000;
 
+const allBlogsData = require("./src/all_blogs")
+
 app.listen(port, () => { console.log(`Listening on port ${port}`); });
+
+app.get('/api/blog/all', (req, res) => {
+    res.send(allBlogsData.allBlogsData);
+});
 
 app.get('/api/express_backend', (req, res) => {
     console.log(`Got a hit. Req: ${req}`);
